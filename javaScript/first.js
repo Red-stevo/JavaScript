8539dc56e74a09cfc28ba9b9fdd9cdac5e0a4fca
Redstevo <stephen.muiru22@students.dkut.ac.ke>
2024-01-08 20:30:44 +0300

    working on javascript

---
+++ javaScript/first.js
@@ -0,0 +1,15 @@
+console.log("Heey there steve..>>>")
+
+
+
+//method in java Script
+findArea = (length, width) => {
+    const area = length * width;
+
+    return area;
+}
+
+console.log("The ar\beea is \v " + findArea(20,30));
+
+
+
