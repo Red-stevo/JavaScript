34bfef2157c4218becb8a819156503b80eac190c
Redstevo <stephen.muiru22@students.dkut.ac.ke>
2024-01-09 07:57:58 +0300

    Js Objects

---
+++ javaScript/Objects.js
@@ -0,0 +1,18 @@
+let alien = {
+    name : "stephen Muiru",
+    tech : "Java Script",
+    'work exp' : "5 years",
+    laptop : {
+        brand : "HP",
+        ram : "8GB",
+        HDD : "500GB",
+        CPU : "Core I5"
+    }
+}
+
+
+//to access the values we used the . or [] operators.
+
+console.log(alien.laptop)
+
+console.log(alien['work exp'])
